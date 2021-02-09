7e4559a60b7f8fa1c1a26e596ed5fe4fd64aea8e
Dashnyam <lord_dashka277@yahoo.com>
2021-02-09 21:16:09 +0800

    redux suulgav

--- src/index.js
+++ src/index.js
@@ -2,10 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./PageComponents/App/App";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import reducer from "./redux/reducer/reducer";
+
+const store = createStore(reducer);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+
   document.getElementById("root")
 );
---
+++ src/redux/reducer/reducer.js
@@ -0,0 +1,9 @@
+initialState = {
+  haha: false,
+};
+
+const reducer = (state = initialState, action) => {
+  return state;
+};
+
+export default reducer;
