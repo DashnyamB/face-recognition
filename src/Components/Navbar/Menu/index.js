2610229048b34935168c4c346ec1e8580fe83f46
Dashnyam <lord_dashka277@yahoo.com>
2021-03-21 23:28:04 +0800

    navbariig zasav

--- src/Components/Navbar/Menu/index.js
+++ src/Components/Navbar/Menu/index.js
@@ -3,17 +3,23 @@ import "./style.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 import MenuItem from "../../MenuItem";
+import { Link } from "react-router-dom";
 
 export default function Menu() {
   const [menus, setMenu] = useState(["home", "services", "team", "contact us"]);
   return (
     <div className="menuf">
-      <div className="menuf__icon">
-        <FontAwesomeIcon icon={faUser} />
-      </div>
       {menus.map((menu) => {
         return <MenuItem key={menu} text={menu} />;
       })}
+      <div className="menuf__icon">
+        <Link to="/login">
+          <FontAwesomeIcon
+            icon={faUser}
+            style={{ color: "#fff", fontSize: "1.5rem" }}
+          />
+        </Link>
+      </div>
     </div>
   );
 }
