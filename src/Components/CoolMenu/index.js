16c6f766b15891f4f629f55148671222225791bf
dashnyam <lord_dashka277@yahoo.com>
2021-04-10 22:04:29 +0800

    cooMenu zasav

--- src/Components/CoolMenu/index.js
+++ src/Components/CoolMenu/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import "./style.scss";
 import bimg from "../../assets/image/cool-menu-background-2560x1600.jpg";
@@ -9,71 +9,84 @@ const CoolMenu = (props) => {
   const toggleCoolMenu = () => {
     props.toggleCoolMenu();
   };
-  const card = document.querySelector(".containz--cool");
-  const cont = document.querySelector(".containz");
-  if (cont && card) {
-    //Moving animation event
-    cont.addEventListener("mousemove", (e) => {
-      let xAxis = (window.innerWidth / 2 - e.pageX) / 50;
-      let yAxis = (window.innerHeight / 2 - e.pageY) / 50;
-      card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+
+  useEffect(() => {
+    const body = document.body;
+    const scrollY = document.documentElement.style.getPropertyValue(
+      "--scroll-y"
+    );
+    if (props.show) {
+      body.style.position = "fixed";
+
+      body.style.top = `-${scrollY}`;
+    } else {
+      body.style.position = "";
+      body.style.top = "";
+      window.scrollTo(0, parseInt(scrollY || "0") * -1);
+    }
+    window.addEventListener("scroll", () => {
+      document.documentElement.style.setProperty(
+        "--scroll-y",
+        `${window.scrollY}px`
+      );
     });
-    //Animate in
-    cont.addEventListener("mouseenter", (e) => {
-      card.style.transition = "none";
-    });
-    //Animate out
-    cont.addEventListener("mouseleave", (e) => {
-      card.style.transition = "all 0.5s ease";
-      card.style.transform = `rotateY(0deg) rotateX(0deg)`;
-    });
-  }
+    const container = document.querySelector(".cool-menu__wrapper");
+    const content = document.querySelector(".cool-menu__content");
+    const texts = document.querySelectorAll(".cool-menu__text");
+    const icon = document.querySelector(".cool-menu__exit-icon");
+    if (container && content && icon && texts) {
+      container.addEventListener("mousemove", (e) => {
+        let xAxis = (window.innerWidth / 2 - e.pageX) / 100;
+        let yAxis = (window.innerHeight / 2 - e.pageY) / 100;
+        content.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+      });
+      container.addEventListener("mouseenter", (e) => {
+        content.style.transition = "none";
+        texts.forEach((text) => {
+          text.style.transform = `translateZ(3.5rem)`;
+        });
+        icon.style.transform = `translateZ(1.5rem)`;
+      });
+      container.addEventListener("mouseleave", (e) => {
+        content.style.transition = "all 0.5s ease";
+        content.style.transform = `rotateY(0deg) rotateX(0deg)`;
+        texts.forEach((text) => {
+          text.style.transform = `translateZ(0)`;
+        });
+        icon.style.transform = `translateZ(0)`;
+      });
+    }
+  }, [props.show]);
 
   return (
-    <div className={`cool-menu ${props.show ? "cool-menu--show" : ""}`}>
-      <FontAwesomeIcon
-        onClick={toggleCoolMenu}
-        icon={faTimes}
-        style={{ color: "#fff", fontSize: "1.5rem" }}
-        className="x"
-      />
-      <div className="containz">
+    <div className={`cool-menu  ${props.show ? "cool-menu--show" : ""}`}>
+      <div className="cool-menu__wrapper">
         <div
-          className="containz--cool"
-          style={{
-            backgroundImage: `url("${bimg}")`,
-            backgroundSize: "cover",
-            backgroundPosition: "center center",
-            backgroundRepeat: "no-repeat",
-          }}
+          className="cool-menu__content"
+          style={{ background: `url(${bimg})`, backgroundSize: "cover" }}
         >
-          <div className="left--info">
-            <div className="left--info--email">
-              <div>-EMAIL</div>
-              <div className="left--info--email--small">info@MATRIX.com</div>
+          <div className="cool-menu__exit-icon">
+            <FontAwesomeIcon
+              onClick={toggleCoolMenu}
+              icon={faTimes}
+              style={{ color: "#fff", fontSize: "1.8rem" }}
+            />
+          </div>
+          <div className="cool-menu__content__contact">
+            <div className="cool-menu__content__contact__email">
+              <h2 className="cool-menu__text">Email</h2>
+              <p className="cool-menu__text">teamone@facereco.com</p>
             </div>
-            <div className="left--info--phone">
-              <div>-PHONE</div>
-              <div className="left--info--phone--small">123456789</div>
-            </div>
-            <div className="left--info--location">
-              <div>-ULAANBAATAR</div>
-              <div className="left--info--location--small">
-                Han-Uul 3-r khoroo
-              </div>
-              <div className="left--info--location--small">
-                Uildverchnii gudamj
-              </div>
+            <div className="cool-menu__content__contact__phone">
+              <h2 className="cool-menu__text">Phone</h2>
+              <p className="cool-menu__text">+976 99009900</p>
             </div>
           </div>
-          <div className="right--info">
-            <div className="right--info--bigone">HOME</div>
-            <div className="right--info--bigone">SERVICES</div>
-            <div className="right--info--bigone">TEAM</div>
-            <div className="right--info--bigone">CONTACT US</div>
-            <div className="right--info--smallone">instagram</div>
-            <div className="right--info--smallone">twitter</div>
-            <div className="right--info--smallone">facebook</div>
+          <div className="cool-menu__content__menu">
+            <h1 className="cool-menu__text">home</h1>
+            <h1 className="cool-menu__text">Services</h1>
+            <h1 className="cool-menu__text">Team</h1>
+            <h1 className="cool-menu__text">Contact us</h1>
           </div>
         </div>
       </div>
