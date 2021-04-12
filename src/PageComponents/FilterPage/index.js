282cca27b82bd0aeaa037b751ed832570d0570df
dashnyam <lord_dashka277@yahoo.com>
2021-04-12 21:46:53 +0800

    filterPage responsive

--- src/PageComponents/FilterPage/index.js
+++ src/PageComponents/FilterPage/index.js
@@ -1,25 +1,70 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Logo from "../../Components/Navbar/Logo";
+import vid from "../../assets/video/Filterpage.mp4";
 import "./style.scss";
+import { useState } from "react/cjs/react.development";
 const FilterPage = (props) => {
   console.log(props.match.params.id);
+  const [width, setWidth] = useState(720);
+  const [height, setHeight] = useState(560);
+  useEffect(() => {
+    const videoContaiter = document.querySelector(
+      ".filtera-page__content__video"
+    );
+
+    // window.addEventListener("resize", () => {});
+    function aa() {
+      setWidth(window.innerWidth);
+    }
+    window.onresize = aa;
+
+    if (videoContaiter) {
+      if (window.innerWidth < 520) {
+        videoContaiter.style.width = `${
+          window.innerWidth - window.innerWidth / 3
+        }px`;
+        videoContaiter.style.height = `${
+          (window.innerWidth - window.innerWidth / 3) * 2
+        }px`;
+      } else if (window.innerWidth > 1200) {
+        videoContaiter.style.width = `${
+          window.innerWidth / 2 - window.innerWidth / 5
+        }px`;
+        videoContaiter.style.height = `${
+          (window.innerWidth / 2 - window.innerWidth / 5) * 1.2
+        }px`;
+      }
+    }
+  }, [width]);
   return (
-    <section className="filter-page">
-      <div className="filter-page__container">
-        <div className="filter-page__logo">
+    <section className="filtera-page">
+      <div className="filtera-page__container">
+        <div className="filtera-page__logo">
           <Logo />
         </div>
-        <div className="filter-page__content">
-          <div className="filter-page__content__description">
-            <div className="filter-page__content__description__text">
+
+        <div className="filtera-page__content">
+          <div className="filtera-page__content__description">
+            <div className="filtera-page__content__description__text">
               ldsal;dkl;akf;kla;skdasdjashdjkhasjkdhsadkjasdsdjkahduiwqydsjka
             </div>
-            <div className="filter-page__content__description__sep"></div>
-            <div className="filter-page__content__description__name">
+            <div className="filtera-page__content__description__sep"></div>
+            <div className="filtera-page__content__description__name">
               #TEAMMATRIX
             </div>
           </div>
-          <div className="filter-page__content__video"></div>
+          <div className="filtera-page__content__video">
+            <video
+              style={{ backgroundColor: "green" }}
+              className="filtera__video"
+              width={width}
+              height={height}
+              autoPlay={true}
+              muted
+            >
+              <source src={vid}></source>
+            </video>
+          </div>
         </div>
       </div>
     </section>
